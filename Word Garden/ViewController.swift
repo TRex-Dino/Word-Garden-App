cddc56a66cad97efe05ff0d27c4e714e26ada6a7
TRex-Dino <dmitrysinkov@gmail.com>
2021-04-23 11:17:48 +0300

    add constraints and add iboutlet

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var wordGuessedLabel: UILabel!
+    @IBOutlet var wordsMissedLabel: UILabel!
+    @IBOutlet var wordsRemainingLabel: UILabel!
+    @IBOutlet var wordsInGameLabel: UILabel!
+    
+    
+    @IBOutlet var wordBeingRevealedLabel: UILabel!
+    
+    @IBOutlet var guessedLetterTextField: UITextField!
+    @IBOutlet var guessLetterButton: UIButton!
+    
+    @IBOutlet var playAgainButton: UIButton!
+    @IBOutlet var gameStatusMessageLabel: UILabel!
+    
+    @IBOutlet var flowerImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
 }
 
